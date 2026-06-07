4e7f3c2b6a76de31bebca351db5db6124f488b3b
shampa <mondal.shampa3105@gmail.com>
2026-06-07 15:37:57 +0530

    Add backend server

--- server/src/index.js
+++ server/src/index.js
@@ -12,11 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "*",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
